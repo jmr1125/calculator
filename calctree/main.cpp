4c35c9f092956f2e167796f74ed3a5ef7b3547eb
jiangmingrui <jmr1125@163.com>
2022-05-15 16:32:19 +0800

    use shared_ptr before void*

--- calctree/main.cpp
+++ calctree/main.cpp
@@ -1,46 +1,71 @@
 #include "mainlib.cpp"
 #include <stack>
 
+template<typename _T>
+const _T as_const(_T val){
+  return val;
+};
+
 calctree::calctree(){
-	l=r=NULL;
+  //l=r=nullptr;
+  //l=r=NULL;
 	//val=NULL;
 }
+
+calctree::calctree(const calctree& rsh){
+  l=rsh.l;
+  r=rsh.r;
+  val=rsh.val;
+};
+calctree::calctree(const calctree&& rsh){
+  l=rsh.l;
+  r=rsh.r;
+  val=rsh.val;
+}
+
 calctree::~calctree(){
-	delete l;
-	delete r;
-	delete this;
+  //delete l;
+  //delete r;
+  //delete this;
 }
 
 
-calctree* build(vector<variant<int,char> > exp){
-	stack<calctree*> num;
-	stack<calctree*> opt;
+calctreeP build(vector<variant<int,char> > exp){
+  stack<calctreeP> num;
+  stack<calctreeP> opt;
 	{
-	  calctree *p=new(calctree);
-		p->val='(';
-		opt.push(p);
-		exp.push_back(')');
+	  // calctree *p=new(calctree);
+	  // 	p->val='(';
+	  // 	opt.push(p);
+	  // 	exp.push_back(')');
+	  auto p=make_shared<calctree>();
+	  p->val='(';
+	  opt.push(p);
+	  exp.push_back(')');
 	}
 	for(auto & i : exp){
 		if(i.index()==0){
-		  calctree *p=new(calctree);
+		  // calctree *p=new(calctree);
+		  auto p=make_shared<calctree>();
 			p->val=i;
 			num.push(p);
 		}else{
 		  if(opt.empty()||level(get<char>(i))>=level(get<char>(opt.top()->val))){
 		  // char optc=i;
 		  // if(opt.empty()||level(optc)>=level(opt.top())){
-		    calctree *p=new(calctree);
+		    // calctree *p=new(calctree);
+		    auto p=make_shared<calctree>();
 				p->val=i;
 				opt.push(p);
 			}else{
 		    while(level(get<char>(opt.top()->val))>level(get<char>(i))){
-					calctree *optp=opt.top();opt.pop();
-					calctree *num1p=num.top();num.pop();
-					calctree *num2p=num.top();num.pop();
-					optp->l=num1p;
-					optp->r=num2p;
-					num.push(optp);
+					// calctree *optp=opt.top();opt.pop();
+					// calctree *num1p=num.top();num.pop();
+					// calctree *num2p=num.top();num.pop();
+					// optp->l=num1p;
+					// optp->r=num2p;
+					// num.push(optp);
+		      num.push(opt.top());
 				}
 			}
 		}
--- calctree/mainlib.cpp
+++ calctree/mainlib.cpp
@@ -1,19 +1,37 @@
 #include<variant>
 #include<vector>
 #include<stack>
+#include<memory>
+#include<utility>
 //#include<compare>
 using std::variant;
 using std::vector;
 using std::stack;
 using std::get;
+using std::unique_ptr;
+using std::make_unique;
+using std::unique;
+using std::shared_ptr;
+using std::swap;
+using std::make_shared;
+//using std::as_const;
+using std::move;
+template<typename _T>
+const _T as_const(_T);
+
+struct calctree;
+using calctreeP = shared_ptr<calctree>;
 
 struct calctree{
   calctree();
+  calctree(const calctree&);
+  calctree(const calctree&&);
   ~calctree();
   variant<int,char> val;
-  calctree *l,*r;
+  //calctree *l,*r;
+  calctreeP l,r;
 };
 
-calctree* build(vector<variant<int,char> >);
+calctreeP build(vector<variant<int,char> >);
 
 int level(char opt);
